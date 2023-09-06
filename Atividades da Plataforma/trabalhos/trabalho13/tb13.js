91c57e33f8dc6c9d392f19144389e46a20101dd3
João Vitor Freitas <joaovitorgfreitas@gmail.com>
2023-09-06 13:33:01 -0300

    criado tb13

---
+++ Atividades da Plataforma/trabalhos/trabalho13/tb13.js
@@ -0,0 +1,11 @@
+class Caneta {
+    constructor(cor, tamanho, tampada){
+        this.cor = cor
+        this.tamanho = tamanho
+        this.tampada = tampada
+    }
+    static escrever(){
+        console.log("Escrevendo...")
+    }
+}
+Caneta.escrever()
