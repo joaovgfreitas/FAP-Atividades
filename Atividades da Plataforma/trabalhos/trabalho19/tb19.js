8b9972aac9c1ff7458d8192cb8d6902413be86b9
João Vitor Freitas <joaovitorgfreitas@gmail.com>
2023-09-15 09:03:21 -0300

    criado tb 19

---
+++ Atividades da Plataforma/trabalhos/trabalho19/tb19.js
@@ -0,0 +1,5 @@
+const apresentacao = " Olá, me chamo João!"
+console.log(apresentacao.toLowerCase())
+console.log(apresentacao.toUpperCase())
+console.log(apresentacao.trim())
+console.log(apresentacao.length)
