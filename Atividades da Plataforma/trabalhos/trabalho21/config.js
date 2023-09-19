f2a2ef41b6ec4c1434b3e228bb44d593831a6147
João Vitor Freitas <joaovitorgfreitas@gmail.com>
2023-09-19 10:09:15 -0300

    criado tb21

---
+++ Atividades da Plataforma/trabalhos/trabalho21/config.js
@@ -0,0 +1,10 @@
+//config.js
+module.exports = {
+    development: {
+        username: "Joaovgf",
+        password: "12345",
+        database: "Primeiro Banco",
+        host: "localhost",
+        dialect: "mysql"
+    }
+}
---
+++ Atividades da Plataforma/trabalhos/trabalho21/migrations/20230919125617-create_produto.js
@@ -0,0 +1,38 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Produtos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      preco: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+      descricao: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }) 
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Produtos')
+  }
+};
