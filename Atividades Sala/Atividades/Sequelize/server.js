5fbb4a321b69027e3f93f1505a1f13913195d565
João Vitor Freitas <joaovitorgfreitas@gmail.com>
2023-10-17 10:45:34 -0300

    alterado

--- Atividades Sala/Atividades/Sequelize/server.js
+++ Atividades Sala/Atividades/Sequelize/server.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const app = express()
 const database = require('./db')
 const bodyParser = require('body-parser')
+const User = require('usermodel')
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(routes)
