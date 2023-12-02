4a77f155b29e8dfcb79646a672e6e8d76d330da2
João Vitor Freitas <joaovitorgfreitas@gmail.com>
2023-12-02 09:55:58 -0300

    Update routes.js

---
+++ Atividades Sala/Atividades/BD-ativ01/routes.js
@@ -0,0 +1,7 @@
+const {Router} = require("express");
+const UserController = require('./Controller/userController')
+
+const router = Router();
+const userController = new UserController();
+router.post("/criar", userController.create);
+module.exports = router;
