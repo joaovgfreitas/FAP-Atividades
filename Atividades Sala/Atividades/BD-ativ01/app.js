baa52125063c1e396fae7d7c4b296ccf3f4519a8
João Vitor Freitas <joaovitorgfreitas@gmail.com>
2023-12-02 10:02:32 -0300

    Update app.js

--- Atividades Sala/Atividades/BD-ativ01/app.js
+++ Atividades Sala/Atividades/BD-ativ01/app.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 const connection = require('./database')
+const Route = require('./routes')
+const app = express();
+
+app.use(express.json());
+app.use(Route)
 
 connection.connect((err) =>{
     if(err){
